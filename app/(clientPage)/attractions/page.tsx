5828da0d2dd223f056f27c82c42cf799fe9cd20f
Asim Poudel <bipulp49@gmail.com>
2025-12-30 18:53:12 +0545

    feat(addbutton): add new destination and activity logic

--- app/(clientPage)/attractions/page.tsx
+++ app/(clientPage)/attractions/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 //@ts-nocheck
 const Attractions = () => {
   return (
-    <div className="px-24 flex flex-col gap-8">
+    <div className="px-24 flex flex-col gap-8 py-8">
       <div className="flex justify-between items-center font-semi-bold">
         <Link href={"/transfers"}> {"<"} Back</Link>
         <div className="flex flex-col items-center gap-2">
@@ -21,6 +21,17 @@ const Attractions = () => {
         <AttractionsBox />
         <AttractionsBox />
         <AttractionsBox />
+      </div>
+      <div className="flex justify-between gap-12">
+        <Link
+          href={"/customItineries"}
+          className="border-sky-500 border-1 border-sky-500 rounded-lg w-1/2 text-center text-sky-500 bg-gray-100 py-2 text-lg hover:cursor-pointer hover:font-bold"
+        >
+          Skip and Customize
+        </Link>
+        <div className="bg-blue-500 rounded-lg text-center text-lg py-2 w-1/2 text-white hover:cursor-pointer hover:font-bold">
+          Continue
+        </div>
       </div>
     </div>
   );
--- app/components/ItineryBox.tsx
+++ app/components/ItineryBox.tsx
@@ -1,41 +1,70 @@
 //@ts-nocheck
-
+"use client";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { CiCirclePlus } from "react-icons/ci";
-const ItinerayBox = ({dayNumber}) => {
-    return (
-        <div className="shadow-xl rounded-lg px-4 py-4 flex flex-col gap-4">
-            <div className="flex justify-between">
-                <div className=" text-[20px] font-bold text-sky-500">
-                    Day {dayNumber}
-                </div>
-                <div>
-                    <RiDeleteBin6Line size={24} color="red"/>    
-                </div>
-            </div>
-            <div className="flex flex-col gap-2">
-                <div className="flex flex-col gap-1">
-                    <div>
-                        Destination
-                    </div>
-                    <div>
-                        <input type="text" placeholder="ktm" className="w-full border-1 rounded-lg px-2 py-1 text-sm border-gray-200" />
-                    </div>
-                </div>
-                 <div className="flex flex-col gap-1">
-                    <div>
-                        Activities & Plans
-                    </div>
-                    <div>
-                        <input type="text" placeholder="choose plans" className="w-full border-1 rounded-lg px-2 py-1 text-sm border-gray-200" />
-                    </div>
-                </div>
+import { useState } from "react";
+const ItinerayBox = ({ dayNumber }) => {
+  const [numberOfTextBox, setNumberOfTextBox] = useState([
+    {
+      id: 1,
+    },
+  ]);
+  const handlePlusbutton = () => {
+    setNumberOfTextBox([
+      ...numberOfTextBox,
+      {
+        id: numberOfTextBox[numberOfTextBox.length - 1].id + 1,
+      },
+    ]);
+  };
 
-            </div>
-            <div className="flex justify-end text-sky-500">
-                <CiCirclePlus size={40}/>
-            </div>
+  const handleDeleteButton = (id) => {
+    console.log(id);
+    const newTextbox = numberOfTextBox.filter((textBox) => textBox.id !== id);
+    setNumberOfTextBox(newTextbox);
+  };
+  return (
+    <div className="shadow-xl rounded-lg px-4 py-4 flex flex-col gap-4">
+      <div className="flex justify-between">
+        <div className=" text-[20px] font-bold text-sky-500">
+          Day {dayNumber}
         </div>
-    )
-}
-export default ItinerayBox
+      </div>
+      {numberOfTextBox.map((eachTextBox) => {
+        return (
+          <div className="flex flex-col gap-2" key={eachTextBox.id}>
+            <div className="flex flex-col gap-1">
+              <div className="flex justify-between">
+                <div>Destination</div>
+                <div onClick={() => handleDeleteButton(eachTextBox.id)}>
+                  <RiDeleteBin6Line size={20} color="red" />
+                </div>
+              </div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="ktm"
+                  className="w-full border-1 rounded-lg px-2 py-1 text-sm border-gray-200"
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-1">
+              <div>Activities & Plans</div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="choose plans"
+                  className="w-full border-1 rounded-lg px-2 py-1 text-sm border-gray-200"
+                />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <div className="flex justify-end text-sky-500" onClick={handlePlusbutton}>
+        <CiCirclePlus size={40} />
+      </div>
+    </div>
+  );
+};
+export default ItinerayBox;
